24454c8732a5ab0969e6d76b7aac2c8f6c9c636a
JuanDavid <HOME@DESKTOP-SHKVMKP>
2025-09-17 17:43:34 -0400

    agregue aguacate

---
+++ Pages/Inventory/aguacate/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaGestionInventario.Pages.Inventory.aguacate
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
