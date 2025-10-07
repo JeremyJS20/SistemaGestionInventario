b9727f79946d87d3c562a174ef1725a9455e0f8f
gpena52 <g.pena52@unapec.edu.do>
2025-10-07 00:29:04 -0400

    Form completed

--- Pages/Inventory/Articles.cshtml.cs
+++ Pages/Inventory/Articles.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using SistemaGestionInventario.Data;
 using SistemaGestionInventario.DTOs;
 using SistemaGestionInventario.Pages.Shared.Types;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SistemaGestionInventario.Pages.Inventory
 {
@@ -21,6 +22,9 @@ namespace SistemaGestionInventario.Pages.Inventory
 
         public List<SelectListItem> Categories { get; set; }
         public List<SelectListItem> States { get; set; }
+        public int Total { get; set; }
+        public int Activos { get; set; }
+        public int StockBajo { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -40,25 +44,33 @@ namespace SistemaGestionInventario.Pages.Inventory
                 new SelectListItem{ Value = "1", Text = "Activo" },
                 new SelectListItem{ Value = "0", Text = "Inactivo" }
             };
+
+            await RefreshData();
         }
 
         public async Task<IActionResult> OnPostEndpoint()
         {
+            Dictionary<string, string> errors = new Dictionary<string, string>();
             if (!ModelState.IsValid)
             {
-                var errors = ModelState
+                errors = ModelState
                     .Where(kvp => kvp.Value.Errors.Count > 0)
                     .ToDictionary(
                         kvp => kvp.Key,
                         kvp => kvp.Value.Errors.First().ErrorMessage
                     );
-
-                return new JsonResult(new { success = false, errors });
             }
+
+            bool existsCode = await _context.Articles.Where(a => a.Code.ToUpper() == (ArticleDto.Code ?? "").ToUpper()).AnyAsync();
+
+            if (existsCode) errors.Add("ArticleDto.Code", "Este codigo ya existe");
+            if (ArticleDto.Category == 0) errors.Add("ArticleDto.Category", "Esta categoria no es valida");
+
+            if (errors.Any()) return new JsonResult(new { success = false, errors });
 
             await _context.Articles.AddAsync(new Models.Article
             {
-                Code = ArticleDto.Code,
+                Code = ArticleDto.Code.ToUpper(),
                 Category = ArticleDto.Category,
                 Name = ArticleDto.Name,
                 Description = ArticleDto.Description,
@@ -70,7 +82,16 @@ namespace SistemaGestionInventario.Pages.Inventory
 
             await _context.SaveChangesAsync();
 
+            await RefreshData();
+
             return new JsonResult(new { success = true });
+        }
+
+        public async Task RefreshData()
+        {
+            Total = await _context.Articles.CountAsync();
+            Activos = await _context.Articles.Where(a => a.State).CountAsync();
+            StockBajo = await _context.Articles.Where(a => a.Stock <= a.MinimumStock).CountAsync();
         }
     }
 }
